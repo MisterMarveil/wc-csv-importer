853aae4150b78ad542fa112a6632156af7d6f924
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-02-05 20:11:06 +0100

    added image handler

--- includes/class-wc-csv-product-handler.php
+++ includes/class-wc-csv-product-handler.php
@@ -24,6 +24,35 @@ class WC_CSV_Product_Handler {
         $product->set_sku($data['sku']);
         $product->set_description($data['description']);
         $product->set_regular_price($data['price']);
+
+        // Télécharger et ajouter l'image principale
+        if (!empty($data['main_image_url'])) {
+            $this->set_product_image($product, $data['main_image_url']);
+        }
+
+        // Ajouter la galerie d'images
+        if (!empty($data['images_csv'])) {
+            $this->set_product_gallery($product, explode('|', $data['images_csv']));
+        }
+
         $product->save();
+    }
+
+    private function set_product_image($product, $image_url) {
+        $attachment_id = media_sideload_image($image_url, 0, '', 'id');
+        if (!is_wp_error($attachment_id)) {
+            $product->set_image_id($attachment_id);
+        }
+    }
+
+    private function set_product_gallery($product, $image_urls) {
+        $gallery_ids = [];
+        foreach ($image_urls as $image_url) {
+            $attachment_id = media_sideload_image($image_url, 0, '', 'id');
+            if (!is_wp_error($attachment_id)) {
+                $gallery_ids[] = $attachment_id;
+            }
+        }
+        $product->set_gallery_image_ids($gallery_ids);
     }
 }
