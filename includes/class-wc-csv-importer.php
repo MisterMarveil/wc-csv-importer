c0d3d6a7ac17e439b02dd76a346c9955e2475842
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-02-05 19:36:18 +0100

    let the csv file being imported form a url

--- includes/class-wc-csv-importer.php
+++ includes/class-wc-csv-importer.php
@@ -1,6 +1,7 @@
  
 <?php
 
+
 // Définition des classes
 class WC_CSV_Importer {
     public function __construct() {
@@ -14,10 +15,11 @@ class WC_CSV_Importer {
 
     public function render_import_page() {
         echo '<div class="wrap"><h1>Importation CSV WooCommerce</h1>';
-        echo '<form method="post" action="'.admin_url('admin-post.php').'" enctype="multipart/form-data">';
+        echo '<form method="post" action="'.admin_url('admin-post.php').'">';
         echo '<input type="hidden" name="action" value="wc_csv_import">';
-        echo '<input type="file" name="csv_file" required />';
-        echo '<input type="submit" name="import_csv" value="Importer" class="button button-primary" />';
+        echo '<label for="csv_url">URL du fichier CSV :</label> ';
+        echo '<input type="text" name="csv_url" id="csv_url" required style="width: 100%; max-width: 600px;" />';
+        echo '<br><br><input type="submit" name="import_csv" value="Importer" class="button button-primary" />';
         echo '</form></div>';
     }
 
@@ -26,14 +28,21 @@ class WC_CSV_Importer {
             wp_die(__('Vous n’avez pas la permission d’effectuer cette action.'));
         }
 
-        if (!isset($_FILES['csv_file']) || empty($_FILES['csv_file']['tmp_name'])) {
-            wp_die(__('Aucun fichier n’a été téléchargé.'));
+        if (!isset($_POST['csv_url']) || empty($_POST['csv_url'])) {
+            wp_die(__('Aucune URL de fichier CSV spécifiée.'));
         }
 
-        set_time_limit(0);
-        $csv_file = $_FILES['csv_file']['tmp_name'];
+        $csv_url = esc_url_raw($_POST['csv_url']);
+        $csv_file = download_url($csv_url);
+
+        if (is_wp_error($csv_file)) {
+            wp_die(__('Erreur lors du téléchargement du fichier CSV.'));
+        }
+
         $handler = new WC_CSV_Product_Handler();
         $handler->import_products($csv_file);
+
+        unlink($csv_file); // Supprime le fichier après traitement
 
         wp_redirect(admin_url('admin.php?page=wc_csv_importer&import_success=1'));
         exit;
