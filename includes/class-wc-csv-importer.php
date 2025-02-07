19583070526275b343eb376c51fa38060c4d074f
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-02-07 21:57:46 +0100

    debugging ajax return

--- includes/class-wc-csv-importer.php
+++ includes/class-wc-csv-importer.php
@@ -115,8 +115,9 @@ class WC_CSV_Importer {
 
        
 
-        $fileContentArray = file($csv_file);
-         wp_send_json(array("success" => true, "message" => $csv_file, "count" => count($fileContentArray)));
+        //$fileContentArray = file($csv_file);
+        $fileContent = file_get_contents($csv_file);
+         wp_send_json(array("success" => true, "message" => $csv_file, "count" => strlen($fileContent)));
         wp_die;
 
         $separators = array();
