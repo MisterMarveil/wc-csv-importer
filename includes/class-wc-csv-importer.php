a394d703438f76d99b0332c91ffc586da7bc004e
Mister Marveil <ewoniewonimerveil@gmail.com>
2025-02-07 12:12:05 +0100

    test chunk breaked csv import

--- includes/class-wc-csv-importer.php
+++ includes/class-wc-csv-importer.php
@@ -82,7 +82,7 @@ class WC_CSV_Importer {
         
         // Import Status
         echo '<div id="import-status" style="margin-top: 10px;"></div>';
-        echo plugins_url( '../assets/js/importer_script.js', __FILE__);
+        echo plugins_url( 'assets/js/importer_script.js');
         echo '</div>';        
         echo '</div>';
     }
